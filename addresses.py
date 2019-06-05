53bbe3ec8e93aa8b6536e391d3a1282e299f2f49
cliveyg <clive@clive.is>
2019-06-05 14:39:25 +0000

    Minor changes

--- addresses.py
+++ addresses.py
@@ -1,8 +1,9 @@
-#from app import app, db, limiter
-from app import create_app, db, limiter
+# addresses.py
+
+from app import create_app, db, limiter, flask_uuid
 
 app = create_app()
 #
 @app.shell_context_processor
 def make_shell_context():
-    return dict(app=app, db=db, limiter=limiter)
+    return dict(app=app, db=db, limiter=limiter, flask_uuid=flask_uuid)
